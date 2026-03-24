96e7379a37b88cf4bd82d63fef1d81d1017e1c9c
LaurencioLm55 <laurenciolopezm@gmail.com>
2026-03-24 17:26:43 -0600

    Subiendo Carptea

---
+++ src/main/java/com/mycompany/sistemapracticasprofesional/SistemaPracticasProfesional.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemapracticasprofesional;
+
+/**
+ *
+ * @author lopez
+ */
+public class SistemaPracticasProfesional {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
