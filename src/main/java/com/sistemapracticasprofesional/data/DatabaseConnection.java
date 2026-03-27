5d98b288c19e1662981138bff339f669ec728ddb
LaurencioLm55 <laurenciolopezm@gmail.com>
2026-03-27 13:06:57 -0600

    CONEXION AGREGADA

--- src/main/java/com/sistemapracticasprofesional/data/DatabaseConnection.java
+++ src/main/java/com/sistemapracticasprofesional/data/DatabaseConnection.java
@@ -1,8 +1,31 @@
 package com.sistemapracticasprofesional.data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
     private static final String URL = "";
     private static final String user = "";
+    private static final String password = "";
     
+    private static Connection connection = null;
     
+    private DatabaseConnection(){}
+    
+    public static Connection getConnection(){
+        
+        try{
+            if(connection == null || connection.isClosed()){
+            
+                connection = DriverManager.getConnection(URL,user,password);
+            
+            }
+        }catch (SQLException e) {
+                System.err.println("Error de SQL al intentar conectar: " + e.getMessage());
+        
+        
+    }
+        return connection;
+    }
 }
