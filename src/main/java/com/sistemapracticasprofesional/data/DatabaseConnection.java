d34f4af18330781fb6a63126977ff15e3c57ad7f
LaurencioLm55 <laurenciolopezm@gmail.com>
2026-03-27 12:17:27 -0600

    creando la conexxion

---
+++ src/main/java/com/sistemapracticasprofesional/data/DatabaseConnection.java
@@ -0,0 +1,8 @@
+package com.sistemapracticasprofesional.data;
+
+public class DatabaseConnection {
+    private static final String URL = "";
+    private static final String user = "";
+    
+    
+}
