f432e6c8645ec3067f54b05e9ee92f8070e47b4d
LaurencioLm55 <laurenciolopezm@gmail.com>
2026-03-27 09:25:39 -0600

    Revert "Acomodando paquetes"
    This reverts commit 175d8948686ecf072b0a2e06f00c7db81f1abd59.

---
+++ src/main/java/com/mycompany/sistemapracticasprofesional/SistemaPracticasProfesional.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemapracticasprofesional;
+
+/**
+ *
+ * @author lopez
+ */
+public class SistemaPracticasProfesional {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("CACA");
+    }
+}
