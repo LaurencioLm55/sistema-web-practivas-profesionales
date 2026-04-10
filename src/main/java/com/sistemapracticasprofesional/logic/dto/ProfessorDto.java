76e09b4823999927c62f0d149a3603494ca856c7
LDarioPL <ldariopadilla@gmail.com>
2026-04-10 14:58:00 -0600

    agregar atributo a dto de professor, de estado

--- src/main/java/com/sistemapracticasprofesional/logic/dto/ProfessorDto.java
+++ src/main/java/com/sistemapracticasprofesional/logic/dto/ProfessorDto.java
@@ -5,23 +5,27 @@ public class ProfessorDto {
     private int professorPersonnelNumber;
     private String professorName;
     private String professorShift;
+    private boolean professorState;
     
     public ProfessorDto() {
     }
 
     public ProfessorDto(int professorPersonnelNumber, String professorName,
-            String professorShift, String professorCourseId) {
+            String professorShift, String professorCourseId,boolean professorState ) {
         this.professorPersonnelNumber = professorPersonnelNumber;
         this.professorName = professorName;
         this.professorShift = professorShift;
+        this.professorState = professorState;
     }
 
     public ProfessorDto(int professorPersonnelNumber, int professorUserId,
-            String professorName, String professorShift, String professorCourseId) {
+            String professorName, String professorShift, String professorCourseId,
+            boolean professorState) {
         this.professorPersonnelNumber = professorPersonnelNumber;
         this.professorUserId = professorUserId;
         this.professorName = professorName;
         this.professorShift = professorShift;
+        this.professorState = professorState;
     }
 
     public int getProfessorUserId() {
@@ -40,6 +44,10 @@ public class ProfessorDto {
         return professorShift;
     }
 
+    public boolean isProfessorState() {
+        return professorState;
+    }
+    
     public void setProfessorUserId(int professorUserId) {
         this.professorUserId = professorUserId;
     }
@@ -55,4 +63,9 @@ public class ProfessorDto {
     public void setProfessorShift(String professorShift) {
         this.professorShift = professorShift;
     }
+
+    public void setProfessorState(boolean professorState) {
+        this.professorState = professorState;
+    }
+    
 }
