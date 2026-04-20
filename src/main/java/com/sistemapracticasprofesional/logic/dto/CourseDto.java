ac89718593f60dbbbeee324b5adbb07594e7142e
leo221313 <leonardopochotl@gmail.com>
2026-04-20 17:24:26 -0600

    Correcion de estilo de codificación del metodo equals en CousrseDto

--- src/main/java/com/sistemapracticasprofesional/logic/dto/CourseDto.java
+++ src/main/java/com/sistemapracticasprofesional/logic/dto/CourseDto.java
@@ -75,50 +75,87 @@ public class CourseDto {
 
     @Override
     public int hashCode() {
+
         final int prime = 31;
         int result = 1;
+        
         result = prime * result + nrc;
         result = prime * result + staffNumber;
         result = prime * result + ((status == null) ? 0 : status.hashCode());
         result = prime * result + ((period == null) ? 0 : period.hashCode());
         result = prime * result + ((section == null) ? 0 : section.hashCode());
         result = prime * result + ((formatFile == null) ? 0 : formatFile.hashCode());
+
         return result;
+
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj){
             return true;
-        if (obj == null)
+        }
+
+        if (obj == null){
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()){
             return false;
+        }
+
         CourseDto other = (CourseDto) obj;
-        if (nrc != other.nrc)
+
+        if (nrc != other.nrc){
             return false;
-        if (staffNumber != other.staffNumber)
+        }
+
+        if (staffNumber != other.staffNumber){
             return false;
+        }
+
         if (status == null) {
-            if (other.status != null)
+
+            if (other.status != null){
                 return false;
-        } else if (!status.equals(other.status))
+            }
+
+        } else if (!status.equals(other.status)){
+
             return false;
+
+        }
         if (period == null) {
-            if (other.period != null)
+
+            if (other.period != null){
                 return false;
-        } else if (!period.equals(other.period))
+            }
+
+        } else if (!period.equals(other.period)){
+         
             return false;
+        
+        }
         if (section == null) {
+
             if (other.section != null)
                 return false;
-        } else if (!section.equals(other.section))
+
+        } else if (!section.equals(other.section)){
+
             return false;
+
+        }
         if (formatFile == null) {
-            if (other.formatFile != null)
+
+            if (other.formatFile != null){
                 return false;
-        } else if (!formatFile.equals(other.formatFile))
+            }
+
+        } else if (!formatFile.equals(other.formatFile)){
+         
             return false;
+
+        }
         return true;
     }
     
