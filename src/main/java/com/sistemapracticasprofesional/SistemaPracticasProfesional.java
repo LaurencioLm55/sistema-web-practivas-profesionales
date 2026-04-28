c5d3b6d093e25edde004fe119665f75ca0e3dec0
LaurencioLm55 <laurenciolopezm@gmail.com>
2026-04-28 01:45:50 -0600

    realizando cambios para la compilacion

--- src/main/java/com/sistemapracticasprofesional/SistemaPracticasProfesional.java
+++ src/main/java/com/sistemapracticasprofesional/SistemaPracticasProfesional.java
@@ -1,6 +1,8 @@
 package com.sistemapracticasprofesional;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,14 +12,28 @@ public class SistemaPracticasProfesional extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/com/sistemapracticasprofesional/presentation/GUIIntern.fxml")
-        );
+        FXMLLoader loader = new FXMLLoader(getCourseViewUrl());
 
         Scene scene = new Scene(loader.load());
         stage.setTitle("Course Registration");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private URL getCourseViewUrl() throws IOException {
+        URL resource = getClass().getResource(
+                "/com/sistemapracticasprofesional/presentation/views/GuiCourses.fxml"
+        );
+
+        if (resource != null) {
+            return resource;
+        }
+
+        File fxmlFile = new File(
+                "src/main/java/com/sistemapracticasprofesional/presentation/views/GuiCourses.fxml"
+        );
+
+        return fxmlFile.toURI().toURL();
     }
 
     public static void main(String[] args) {
